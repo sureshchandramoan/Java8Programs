7d651acf03ad0c16857c3a02fdf616310c1a7299
JC <myjavaarena@gmail.com>
2022-02-20 22:44:15 +0530

    long array and String array to stream and print

--- src/main/java/com/myjavaarena/programs/ConvertArrayToStream.java
+++ src/main/java/com/myjavaarena/programs/ConvertArrayToStream.java
@@ -4,8 +4,17 @@ import java.util.Arrays;
 
 public class ConvertArrayToStream {
     public static void main(String[] args) {
-        int[] values = {1,2,3,4,5,6,7,8,9,10};
+        int[] intValues = {1,2,3,4,5,6,7,8,9,10};
+        Arrays.stream(intValues).forEach(System.out::print);
+        System.out.println("");
+        long[] longValues = { 5,3,7,2,8,6 };
+        Arrays.stream(longValues).forEach(System.out::print);
+        System.out.println("");
+        Arrays.stream(longValues).forEachOrdered(System.out::print);
+        System.out.println("");
+        Arrays.stream(longValues).sorted().boxed().forEach(System.out::print);
+        String[] stringValues = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+       Arrays.stream(stringValues).forEach(System.out::println);
 
-        Arrays.stream(values).forEach(System.out::println);
     }
 }
