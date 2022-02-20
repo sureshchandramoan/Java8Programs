823f45bb40bcc52f7f4ede50bc827e23c48f06ea
JC <myjavaarena@gmail.com>
2022-02-20 23:29:37 +0530

    extract function functional interface

--- src/main/java/com/myjavaarena/programs/StreamMap.java
+++ src/main/java/com/myjavaarena/programs/StreamMap.java
@@ -2,6 +2,8 @@ package com.myjavaarena.programs;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.function.Function;
 
 /**
  * Example for map method in Stream
@@ -9,7 +11,9 @@ import java.util.List;
 public class StreamMap {
     public static void main(String[] args) {
         List<String> stringValues = List.of("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-        stringValues.stream().map(val -> val.toUpperCase() + " ").forEach(System.out::print);
+        Function<String, String> upper = (val) -> val.toUpperCase(Locale.ROOT);
+        Function<String, String> bracket = (val) -> "["+val + "] ";
+        stringValues.stream().map(upper.andThen(bracket)).forEach(System.out::print);
         System.out.println("");
         stringValues.stream().map(val -> val.toLowerCase() + " ").forEach(System.out::print);
     }
